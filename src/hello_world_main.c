61e03cdb5cc08e00c54f4c83e73082cc8ee46e68
ronf-ucb <ronf@berkeley.edu>
2020-12-28 18:33:28 -0800

    added timer example- will be used for high resolution systime

--- src/hello_world_main.c
+++ src/hello_world_main.c
@@ -12,11 +12,12 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+void app_timer(void);  // prototype
 
 void app_main()
-{
+{   double z=3.14159;
     printf("Hello world! 12/28/20 v4\n");
-
+    printf("Floating point 3.14159=%10.3f\n", z);
     /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
@@ -29,6 +30,9 @@ void app_main()
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("About to start timer apps\n");
+    app_timer();
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
